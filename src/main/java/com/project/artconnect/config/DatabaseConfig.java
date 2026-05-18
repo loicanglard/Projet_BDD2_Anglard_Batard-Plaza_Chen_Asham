44369953dea61fa951d12ac57543598ebf4704e7
Esteban BP <150625804+Toxies@users.noreply.github.com>
2026-05-18 23:32:02 +0200

    Update DatabaseConfig.java
    dont look at my password pls

--- src/main/java/com/project/artconnect/config/DatabaseConfig.java
+++ src/main/java/com/project/artconnect/config/DatabaseConfig.java
@@ -4,5 +4,5 @@ public class DatabaseConfig {
     public static final String URL =
             "jdbc:mysql://localhost:3306/bdd2_project?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
     public static final String USER = "root";
-    public static final String PASSWORD = "root";
+    public static final String PASSWORD = "password";
 }
